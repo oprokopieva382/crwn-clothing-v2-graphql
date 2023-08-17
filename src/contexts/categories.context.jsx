417b9f4d7b7130ff927f3521273965052b4e5bcb
Oksana(KsyFate) <oprokopieva382@gmail.com>
2023-08-17 13:37:51 -0500

    added graphQl to the project

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -1,23 +1,31 @@
-import { createContext, useState, useEffect } from 'react';
-
-import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils';
+import { createContext, useState, useEffect } from "react";
+import { gql, useQuery } from "@apollo/client";
 
 export const CategoriesContext = createContext({
   categoriesMap: {},
 });
 
+const COLLECTIONS = gql`
+  query {
+    collections {
+      id
+      title
+      items {
+        id
+        name
+        imageUrl
+        price
+      }
+    }
+  }
+`;
+
 export const CategoriesProvider = ({ children }) => {
   const [categoriesMap, setCategoriesMap] = useState({});
+  const { loading, error, data } = useQuery(COLLECTIONS);
 
-  useEffect(() => {
-    const getCategoriesMap = async () => {
-      const categoryMap = await getCategoriesAndDocuments();
-      setCategoriesMap(categoryMap);
-    };
-
-    getCategoriesMap();
-  }, []);
-
+  console.log("loading", loading);
+  console.log("data", data);
   const value = { categoriesMap };
   return (
     <CategoriesContext.Provider value={value}>
